c98419f78d44c63599ace3ad60c46b0a296e2cac
mnfiguera1@gmail.com <juad7000@gmail.com>
2026-03-01 21:53:04 -0500

    actualizacion de servicios, se agrego un video de fondo, se agregaron imagenes, se modifico el html de reseñas y registro, se modifico el css de contacto, registro y reseñas, se agregaron archivos js para registro y reseñas

---
+++ archivos js/reseñas.js
@@ -0,0 +1,27 @@
+const ratingContainer = document.getElementById('rating');
+const totalStars = 5;
+let rating = 0;
+
+// Crear las estrellas dinámicamente
+for (let i = 0; i < totalStars; i++) {
+  const star = document.createElement('span');
+  star.className = "material-symbols-outlined cursor-pointer hover:scale-110 transition-transform";
+  star.textContent = "star_outline"; // por defecto vacía
+  ratingContainer.appendChild(star);
+
+  // Evento de clic
+  star.addEventListener('click', () => {
+    rating = i + 1;
+    updateStars(rating);
+    console.log("Calificación seleccionada:", rating);
+  });
+}
+
+// Función para actualizar las estrellas
+function updateStars(rating) {
+  const stars = ratingContainer.querySelectorAll('span');
+  stars.forEach((star, index) => {
+    star.textContent = index < rating ? "star" : "star_outline";
+  });
+}
+
