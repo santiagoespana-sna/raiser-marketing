61fb9abee79f58dce0600f9c1ab5b1cd6a55d47b
tatianakcamilo-cell <tatianakcamilo@gmail.com>
2026-03-04 21:47:44 -0500

    actualizacion login

--- archivosjs/login.js
+++ archivosjs/login.js
@@ -1,10 +1,24 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
 function login(event) {
     event.preventDefault();
 
     const correo = document.getElementById("correo").value;
     const password = document.getElementById("password").value;
 
-    const correoCorrecto = "biznova@g";
+    const correoCorrecto = "biznova@gmail.com";
     const passwordCorrecta = "123456";
 
     if (correo === correoCorrecto && password === passwordCorrecta) {
