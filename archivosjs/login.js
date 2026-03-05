a51a8ee362cf6179486a45f7e776fc6690e753ec
tatianakcamilo-cell <tatianakcamilo@gmail.com>
2026-03-05 01:24:17 -0500

    actualizacion tati

--- archivosjs/login.js
+++ archivosjs/login.js
@@ -10,9 +10,22 @@ function login(event) {
     if (correo === correoCorrecto && password === passwordCorrecta) {
 
         localStorage.setItem("isLogged", "true");
-        window.location.href = "dashboar.html";
+        window.location.href = "../dashboar.html";
 
     } else {
         alert("Correo o contraseña incorrectos");
     }
 }
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
